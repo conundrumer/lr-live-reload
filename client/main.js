e000ec27e4d0d047cc260d361905cc803328815c
David Lu <davidelu93@gmail.com>
2018-11-20 02:22:13 -0500

    generate grids

--- client/main.js
+++ client/main.js
@@ -1,12 +1,54 @@
-import test from "./test.js";
+// import test from "./test.js";
 
-const { store } = window;
+const { store, V2 } = window;
+
+const range = (start, end, step = 1) =>
+  Array(end - start)
+    .fill(0)
+    .map((_, i) => i * step + start);
 
 export default function init() {
-  console.log(test())
-  store.dispatch(
-    addLines([{ x1: 0, y1: 0, x2: Math.random() * 100, y2: 100, type: 2 }])
-  );
+  const fps = 40
+  const timeRange = range(4 * fps, 20 * fps);
+  const gridLength = 5;
+  const gridSpan = range(-gridLength, gridLength + 1);
+  const track = getSimulatorTrack(store.getState());
+
+  const getPos = i => track.getRider(i).position;
+  const getTan = i =>
+    new V2(track.getRider(i + 1).position)
+      .sub(track.getRider(i - 1).position)
+      .div(2);
+
+  const lines = [];
+  for (let i of timeRange) {
+    const p1 = getPos(i - 0.5);
+    const p2 = getPos(i + 0.5);
+    const t1 = getTan(i - 0.5);
+    const t2 = getTan(i + 0.5);
+
+    // parallels
+    for (let j of gridSpan) {
+      j += 0.5
+      lines.push({
+        x1: p1.x - t1.y * j,
+        y1: p1.y + t1.x * j,
+        x2: p2.x - t2.y * j,
+        y2: p2.y + t2.x * j,
+        type: 2
+      });
+    }
+    // perpendicular
+    lines.push({
+      x1: p1.x - t1.y * (gridLength + 0.5),
+      y1: p1.y + t1.x * (gridLength + 0.5),
+      x2: p1.x + t1.y * (gridLength - 0.5),
+      y2: p1.y - t1.x * (gridLength - 0.5),
+      type: 2
+    });
+  }
+
+  store.dispatch(addLines(lines));
 }
 
 export function unload() {
@@ -28,3 +70,6 @@ const addLines = line => updateLines(null, line, "ADD_LINES");
 const revertTrackChanges = () => ({
   type: "REVERT_TRACK_CHANGES"
 });
+
+/* selectors */
+const getSimulatorTrack = state => state.simulator.engine;
